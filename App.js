1f7b9e5583e09d5622240d7e3b57adc8907eae3d
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-05-18 23:22:23 -0400

    finsh main screen ui

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image ,TouchableOpacity,Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./assets/images/Bergers.png')}
+        style={styles.MainImage}
+        resizeMode='contain'
+      /> 
+    
+      <Text style={styles.Tilte} > Delcious food  </Text> 
+    
+      <Text style={styles.SubTilte} > we help you to find best and  </Text>
+      <Text style={styles.SubTilte}>delicious food </Text> 
+
+
+      <View style={styles.butoonContent}>
+        <TouchableOpacity style={styles.butoon} >
+          <Text style={{ textAlign:'center' , fontSize:18 , color:'#fff'}} > Get started  </Text>
+        </TouchableOpacity>
+      </View>
+    
+
     </View>
   );
 }
@@ -15,6 +32,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingVertical:60, 
   },
+  MainImage : {
+     width:300,
+     height:300, 
+  },
+  TitleContent : {
+    
+  },
+  Tilte : {
+    fontSize:40,
+    paddingVertical:30,
+  },
+  SubTilte : {
+    fontSize:20,
+    paddingVertical:0,
+    color:'#333',
+
+  }, 
+  butoonContent : {
+    flex:1,
+    justifyContent:'flex-end',
+    alignItems:'center', 
+  },
+  butoon : {
+    backgroundColor:'#F66B0E',
+    width:Dimensions.get('screen').width - 20,
+    borderRadius:30,
+    padding:20,
+  },
+
+
 });
